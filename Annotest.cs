82d32df2ee9480f776424cd77e36874fd0786866
sipra <sipramishra085@gmail.com>
2023-04-13 01:38:12 +0530

    Added UC1

---
+++ Annotest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day22UserResigstrationProblem
+{
+    public class Annotest
+    {
+        public static void Display()
+        {
+            CheckValidation check = new CheckValidation();
+            Console.WriteLine("Enter Your Name");
+            check.Name = Console.ReadLine();
+
+            ValidationContext con = new ValidationContext(check);
+            List<ValidationResult> results = new List<ValidationResult>();
+
+            bool valid = Validator.TryValidateObject(check, con, results, true);
+            if (!valid)
+            {
+                foreach (ValidationResult i in results)
+                {
+                    Console.WriteLine("Member Name : {0}", i.MemberNames);
+                    Console.WriteLine("Error Message : {0}", i.ErrorMessage);
+                }
+            }
+            else
+            {
+                Console.WriteLine(" Name :" + check.Name);
+            }
+        }
+    }
+}
---
+++ CheckValidation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day22UserResigstrationProblem
+{
+    public class CheckValidation
+    {
+        
+       // [Required(ErrorMessage = "Property check {0} is required")]
+        [RegularExpression("^[A-Z]{1}[A-Za-z]{2,}$", ErrorMessage = "Name should be minimum 3 character and start with caps")]
+        // [StringLength(100,MinimumLength =3 ,ErrorMessage = "Name should be minimum 3 character" )]
+        public string Name { get; set; }
+    }
+}
+
