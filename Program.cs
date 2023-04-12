ddc4435b82a5cabc5ad6eeb0384300793055c523
sipra <sipramishra085@gmail.com>
2023-04-13 01:33:59 +0530

    Added Main Program File

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using Day22UserResigstrationProblem;
+
+namespace Day22UserRegistrationProblem
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to User Regestration problem!!");
+            Console.WriteLine("*********************************");
+
+            Annotest.Display();
+        }
+    }
+}
